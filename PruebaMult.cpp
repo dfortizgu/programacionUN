5641ce968b119350cce5503b3c2f18810850829b
David Ortiz <dfortizgu@unal.edu.co>
2018-02-16 23:01:33 -0500

    prueba versiones

--- PruebaMult.cpp
+++ PruebaMult.cpp
@@ -2,7 +2,7 @@
 int main(){
   //Se declara la variable "num" la cual sera el numero utilizado para realizar la multiplicacion
   float num;
-  std::cout << "Escriba un numero y lo multiplicare por 2, puede ser un entero o un decimal!!\n";
+  std::cout << "Escriba un numero y lo multiplicare por 2, puede ser un entero o un decimal!!!!\n";
   //Se usa std::cin >> num para introducir un valor a la variable num pr medio del teclado
   std::cin >> num;
   std::cout << "Su numero es " << num*2;
